1b13cd68460db4f2470235a2e00ad6e9e564d24b
Maru <gmarudev@gmail.com>
2026-06-04 21:04:03 -0300

    Fix: code indentation

--- Src/Domain/Entities/CategoryEnt.ts
+++ Src/Domain/Entities/CategoryEnt.ts
@@ -6,34 +6,38 @@ type CategoryInput = {
 
 export class CategoryEnt {
     private readonly _categoryId: string;
-    private readonly _categoryUserId: string;
     private _categoryName: string;
+    private readonly _categoryUserId: string;
+
     constructor(parCategory: CategoryInput) {
-        if (!parCategory.categoryName) { throw new Error("A category must br inserted.") }
-        const normalizedCategory = parCategory.categoryName.
-            trim().
-            toLowerCase().
-            replace(/\s+/g, " ").
-            split(" ").
-            map(word => word.charAt(0).toUpperCase() + word.slice(1)).
-            join(" ");
-        if (!parCategory.categoryUserId) { throw new Error("A category must belong to a user") }
+        if (!parCategory.categoryName) {
+            throw new Error("A category must be inserted.")
+        }
+        if (!parCategory.categoryUserId) {
+            throw new Error("A category must belong to a user")
+        }
+        const normalizedCategory = parCategory.categoryName.trim().toLowerCase().replace(/\s+/g, " ").split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+
         if (normalizedCategory.length < 3 || normalizedCategory.length > 32) {
             throw new Error("Category must be between 3 and 32 characters");
         }
+
         this._categoryId = parCategory.categoryId;
         this._categoryName = normalizedCategory;
         this._categoryUserId = parCategory.categoryUserId
     }
 
     // Getters:
-    getCategoryId(): number | string | undefined {
+    getCategoryId(): string {
         return this._categoryId;
     }
+
     getCategoryName(): string {
         return this._categoryName;
     }
-    getCategoryUserId(): number | string {
+
+    getCategoryUserId(): string {
         return this._categoryUserId;
     }
+
 }
--- Src/Domain/Value-Objects/PasswordVO.ts
+++ Src/Domain/Value-Objects/PasswordVO.ts
@@ -2,8 +2,12 @@ export class PasswordVO {
     private readonly attPassword: string;
 
     constructor(parPassword: string) {
-        if (!parPassword) { throw new Error("A password must be inserted."); }
-        if (parPassword.length < 8 || parPassword.length > 32) { throw new Error("Password must be between 8 and 32 characters."); }
+        if (!parPassword) {
+            throw new Error("A password must be inserted.");
+        }
+        if (parPassword.length < 8 || parPassword.length > 32) {
+            throw new Error("Password must be between 8 and 32 characters.");
+        }
 
         this.attPassword = parPassword;
     }
